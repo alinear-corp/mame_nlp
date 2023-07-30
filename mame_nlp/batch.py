c182cb0517c45abe5d5cea09fb0e9045a8d452d4
Jun-ya NORIMATSU <jnory@users.noreply.github.com>
2023-07-30 22:33:16 +0900

    Add batch utility

---
+++ mame_nlp/batch.py
@@ -0,0 +1,28 @@
+from typing import Generator, TypeVar, Protocol
+
+T = TypeVar('T')
+
+class CountableWithGetItem(Protocol):
+    def __getitem__(self, item: slice) -> T:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+
+def batch_by_slice(
+        data: CountableWithGetItem,
+        batch_size: int,
+        drop_last: bool=False,
+) -> Generator[T, None, None]:
+    start: int = 0
+    end: int = start + batch_size
+    n: int = len(data)
+
+    while end < n:
+        yield data[start:end]
+        start = end
+        end += batch_size
+
+    if (not drop_last) and start < n:
+        yield data[start:n]
---
+++ mame_nlp/test_batch.py
@@ -0,0 +1,35 @@
+from itertools import zip_longest
+from unittest import TestCase
+
+from mame_nlp.batch import batch_by_slice
+
+
+class TestBatch(TestCase):
+    def test_batch_by_slice_list_drop_last(self):
+        data = list(range(10))
+        reference = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+        ]
+        iterator = zip_longest(
+            batch_by_slice(data, 3, drop_last=True),
+            reference,
+        )
+        for actual, ref in iterator:
+            self.assertEqual(actual, ref)
+
+    def test_batch_by_slice_list_no_drop_last(self):
+        data = list(range(10))
+        reference = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [9],
+        ]
+        iterator = zip_longest(
+            batch_by_slice(data, 3, drop_last=False),
+            reference,
+        )
+        for actual, ref in iterator:
+            self.assertEqual(actual, ref)
